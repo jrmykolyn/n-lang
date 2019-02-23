ed0d11b5bb4419cf4f42724ed88613331a68ece3
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-02-23 17:30:48 -0500

    Add support for tokenization of single line programs

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,3 +1,12 @@
-class Tokenizer {}
+class Tokenizer {
+  static get DELIMITER() {
+    return ' ';
+  }
+
+  tokenize(proc) {
+    return proc.split(Tokenizer.DELIMITER)
+      .map((token) => !isNaN(+token) ? +token : token);
+  }
+}
 
 module.exports = Tokenizer;
--- test/unit/tokenizer.test.js
+++ test/unit/tokenizer.test.js
@@ -11,4 +11,22 @@ describe('Tokenizer', () => {
       expect(new Tokenizer()).to.be.an.instanceof(Tokenizer);
     });
   });
+
+  describe('Instance methods', () => {
+    let tokenizer;
+
+    beforeEach(() => {
+      tokenizer = new Tokenizer();
+    });
+
+    describe('tokenize()', () => {
+      it('should convert a single line program into a collection of tokens', () => {
+        const proc = 'sum 4 3';
+
+        const result = tokenizer.tokenize(proc);
+
+        expect(result).to.eql(['sum', 4, 3]);
+      });
+    });
+  });
 });
